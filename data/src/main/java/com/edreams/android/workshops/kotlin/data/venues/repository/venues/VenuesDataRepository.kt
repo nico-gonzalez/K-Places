73ae526eac187e58d02381650d218d41411bc5b6
Nicolas Gonzalez <nico.gonzalez90@gmail.com>
2017-12-02 15:50:48 +0100

    Move venues repository to its own feature package

--- data/src/main/java/com/edreams/android/workshops/kotlin/data/venues/repository/venues/VenuesDataRepository.kt
+++ data/src/main/java/com/edreams/android/workshops/kotlin/data/venues/repository/venues/VenuesDataRepository.kt
@@ -1,12 +1,12 @@
-package com.edreams.android.workshops.kotlin.data.venues.repository
+package com.edreams.android.workshops.kotlin.data.venues.repository.venues
 
 import com.edreams.android.workshops.kotlin.data.venues.cache.dao.VenuesDao
 import com.edreams.android.workshops.kotlin.data.venues.cache.entity.VenueEntity
 import com.edreams.android.workshops.kotlin.data.venues.remote.ExploreVenuesController
 import com.edreams.android.workshops.kotlin.data.venues.remote.response.VenueResponse
-import com.edreams.android.workshops.kotlin.domain.common.VenuesListProducer
 import com.edreams.android.workshops.kotlin.domain.mapper.Mapper
 import com.edreams.android.workshops.kotlin.domain.model.VenueModel
+import com.edreams.android.workshops.kotlin.domain.repositories.VenuesListProducer
 import com.edreams.android.workshops.kotlin.domain.repositories.VenuesRepository
 import javax.inject.Inject
 
@@ -16,21 +16,20 @@ class VenuesDataRepository @Inject constructor(
     private val exploreVenuesController: ExploreVenuesController,
     private val venuesDao: VenuesDao) : VenuesRepository {
 
-  suspend override fun getVenues(query: String): VenuesListProducer =
-      produce {
-        val normalizedQuery = query.toLowerCase()
-        val cachedVenues = venuesDao.findByQuery(normalizedQuery)
-        if (cachedVenues.isNotEmpty()) {
-          send(cacheMapper.map(cachedVenues))
+  suspend override fun getVenues(query: String): VenuesListProducer = produce {
+    val normalizedQuery = query.toLowerCase()
+    val cachedVenues = venuesDao.findByQuery(normalizedQuery)
+    if (cachedVenues.isNotEmpty()) {
+      send(cacheMapper.map(cachedVenues))
+    }
+    val remoteVenues = remoteMapper.map(
+        exploreVenuesController.exploreVenues(normalizedQuery))
+        .map {
+          it.copy(query = normalizedQuery)
         }
-        val remoteVenues = remoteMapper.map(
-            exploreVenuesController.exploreVenues(normalizedQuery))
-            .map {
-              it.copy(query = normalizedQuery)
-            }
-            .toTypedArray()
+        .toTypedArray()
 
-        venuesDao.clearAndInsert(normalizedQuery, remoteVenues)
-        send(cacheMapper.map(venuesDao.findByQuery(normalizedQuery)))
-      }
+    venuesDao.clearAndInsert(normalizedQuery, remoteVenues)
+    send(cacheMapper.map(venuesDao.findByQuery(normalizedQuery)))
+  }
 }
