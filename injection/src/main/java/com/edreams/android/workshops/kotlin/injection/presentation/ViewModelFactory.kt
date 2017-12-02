6a7994c201cdccde0076ca39c8b02553a599d01b
Nicolas Gonzalez <nico.gonzalez90@gmail.com>
2017-12-02 15:51:29 +0100

    Convert ViewModelFactory to kotlin

---
+++ injection/src/main/java/com/edreams/android/workshops/kotlin/injection/presentation/ViewModelFactory.kt
@@ -0,0 +1,34 @@
+package com.edreams.android.workshops.kotlin.injection.presentation
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import com.edreams.android.workshops.kotlin.injection.scopes.PerApplication
+import javax.inject.Inject
+import javax.inject.Provider
+
+@PerApplication
+class ViewModelFactory @Inject constructor(
+    private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>) : ViewModelProvider.Factory {
+
+  @Suppress("UNCHECKED_CAST")
+  override fun <T : ViewModel> create(modelClass: Class<T>): T {
+    var creator: Provider<out ViewModel>? = creators[modelClass]
+    if (creator == null) {
+      for ((key, value) in creators) {
+        if (modelClass.isAssignableFrom(key)) {
+          creator = value
+          break
+        }
+      }
+    }
+    if (creator == null) {
+      throw IllegalArgumentException("unknown model class " + modelClass)
+    }
+    try {
+      return creator.get() as T
+    } catch (e: Exception) {
+      throw RuntimeException(e)
+    }
+
+  }
+}
