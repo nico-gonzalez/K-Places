ef1c2cec41d65bccb39613c9141f47d517f5ba03
Nicolas Gonzalez <nico.gonzalez90@gmail.com>
2018-01-13 16:27:06 +0100

    Handle exception on net controller when api request fails

--- data/src/main/java/com/edreams/android/workshops/kotlin/data/venues/remote/ExploreVenuesNetController.kt
+++ data/src/main/java/com/edreams/android/workshops/kotlin/data/venues/remote/ExploreVenuesNetController.kt
@@ -2,6 +2,8 @@ package com.edreams.android.workshops.kotlin.data.venues.remote
 
 import com.edreams.android.workshops.kotlin.data.common.remote.FoursquareService
 import com.edreams.android.workshops.kotlin.data.venues.remote.response.VenueResponse
+import retrofit2.HttpException
+import java.io.IOException
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -12,7 +14,13 @@ private const val PHOTOS_COUNT = 1
 class ExploreVenuesNetController @Inject constructor(
     private val service: FoursquareService) : ExploreVenuesController {
 
-  suspend override fun exploreVenues(near: String): List<VenueResponse> =
-      service.exploreVenues(near, RESULTS_LIMIT, venuePhotos = PHOTOS_COUNT)
-          .await().response.groups[0].items.map { it.venue }
+  suspend override fun exploreVenues(near: String): List<VenueResponse> = try {
+    service.exploreVenues(near, RESULTS_LIMIT, venuePhotos = PHOTOS_COUNT)
+        .await().response.groups[0].items.map { it.venue }
+  } catch (http: HttpException) {
+    emptyList()
+  } catch (network: IOException) {
+    emptyList()
+  }
 }
+
